73e686dcfe318da0b1bf1458231196dc40f57ff0
Brahian <teslasapbe@gmail.com>
2020-05-12 00:25:04 -0300

    user authentication

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,22 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Api Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register Api routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "api" middleware group. Enjoy building your Api!
 |
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('auth/login', 'Api\Auth\AuthController@login');
+Route::post('auth/register', 'Api\Auth\AuthController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('user-detail', 'Api\Auth\AuthController@userDetail');
 });
