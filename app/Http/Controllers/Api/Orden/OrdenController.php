2efe81a4aef235b0e62fc8da2704b26be2c4e80b
Isaac <48778675+Rodyu10@users.noreply.github.com>
2020-06-24 04:42:03 -0300

    Sumar/restar stock al crear/cancelar orden

--- app/Http/Controllers/Api/Orden/OrdenController.php
+++ app/Http/Controllers/Api/Orden/OrdenController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Direccion;
 use App\Models\Orden;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Validator;
 use App\Models\Articulo;
 
@@ -49,6 +48,7 @@ class OrdenController extends Controller
         $result = Orden::create($orden);
         foreach ($orden['articulos'] as $articulo_id ){
             $articulo = Articulo::findOrFail($articulo_id['id']);
+            $articulo->update(['stock' => $articulo['stock'] - $articulo_id['cantidad']]);
             $result->articulos()->attach($articulo,['cantidad'=>$articulo_id['cantidad']]);
         }
 
@@ -83,8 +83,13 @@ class OrdenController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $orden = Orden::find($id);
+        $orden = Orden::findOrFail($id);
+        $articulos = $orden->articulos()->get();
 
+        foreach ($articulos as $articulo_id ){
+            $articulo = Articulo::findOrFail($articulo_id['id']);
+            $articulo->update(['stock' => $articulo['stock'] + $articulo_id['pivot']['cantidad']]);
+        }
         $orden->estado = "CANCELADO";
         $orden->save();
         return response()->json(['success' => true, 'orden' => $orden], 200);
